26816a4035b12e23879145f25a45de9aade63b74
qqadi <qaisalqadi2000@gmail.com>
2024-06-11 09:39:30 +0300

    [TAS-02/03] Done, with unit tests

--- src/stack_list.py
+++ src/stack_list.py
@@ -1,6 +1,16 @@
 class StackList:
     def __init__(self): 
-        self.list = []
-    
+        self.list = []  
+
     def push(self, data): #the push'd data will always be index 0
         self.list[:0] = [data]
+
+    def pop(self):
+        if self.is_empty(): ##check if empty
+            return None
+        temp = self.list[0]
+        self.list = self.list[1:] #create a new list from index1 to the end
+        return temp    #assign new list to old list, and return temp value
+
+    def is_empty(self): #if self.list is empty, it returns as false
+        return not self.list #we use not, to reverse the boolean to true
--- src/test_stack.py
+++ src/test_stack.py
@@ -7,6 +7,23 @@ def test_stack_push():
     stack.push(3)
     assert stack.list == [3, 2, 1]
 
+def test_stack_pop():
+    stack = StackList()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    assert stack.pop() == 3
+    assert stack.pop() == 2
+    assert stack.pop() == 1
+    assert stack.pop() == None
+
+def test_stack_is_empty():
+    stack = StackList() 
+    assert stack.is_empty() == True
+    stack.push(1)
+    assert stack.is_empty() == False
 
 if __name__ == "__main__":
     test_stack_push()
+    test_stack_is_empty()
+    test_stack_pop()
